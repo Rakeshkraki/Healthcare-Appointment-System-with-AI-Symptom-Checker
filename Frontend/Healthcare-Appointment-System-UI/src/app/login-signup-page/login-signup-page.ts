720de2ba155d0dd6c5e8af9850fd870ec17632bb
Rakesh K <rakeshkraki3272@gmail.com>
2025-11-11 21:22:05 +0530

    login page design

---
+++ Frontend/Healthcare-Appointment-System-UI/src/app/login-signup-page/login-signup-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-signup-page',
+  imports: [],
+  templateUrl: './login-signup-page.html',
+  styleUrl: './login-signup-page.css',
+})
+export class LoginSignupPage {
+
+}
